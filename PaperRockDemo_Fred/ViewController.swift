a6c3b1c5a4e1adeacef60ab8aa0e02303a8c0511
fred.fu <fjw08199999@gmail.com>
2021-10-10 13:45:01 +0800

    Initial Commit

---
+++ PaperRockDemo_Fred/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PaperRockDemo_Fred
+//
+//  Created by fred fu on 2021/10/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
